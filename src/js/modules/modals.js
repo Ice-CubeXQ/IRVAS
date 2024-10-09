6e6025077c8d50500b01e83d42df4cb6abcbef88
Ice-CubeXQ <dealplug72@gmail.com>
2024-10-09 21:24:30 +0300

    first commit

---
+++ src/js/modules/modals.js
@@ -0,0 +1,51 @@
+const modals = () => {
+  function bindModal(triggerSelector, modalSelector, closeModalSelector) {
+    const modalTriggers = document.querySelectorAll(triggerSelector);
+    const modal = document.querySelector(modalSelector);
+    const closeModalButton = document.querySelector(closeModalSelector);
+
+    modalTriggers.forEach((trigger) => {
+      trigger.addEventListener("click", function (e) {
+        if (e.target) {
+          modal.style.display = "block";
+          document.body.classList.add("modal-open");
+        }
+      });
+    });
+    closeModalButton.addEventListener("click", function (e) {
+      if (e.target) {
+        modal.style.display = "none";
+        document.body.classList.remove("modal-open");
+      }
+    });
+    document.addEventListener("click", function (e) {
+      if (e.target === modal) {
+        modal.style.display = "none";
+        document.body.classList.remove("modal-open");
+      }
+    });
+  }
+
+  function showModal(selector, time) {
+    setTimeout(() => {
+      const modal = document.querySelector(selector);
+      const closeModalButton = document.querySelector(`${selector} .popup_close`);
+
+      modal.style.display = "block";
+      document.body.classList.remove("modal-open");
+
+      document.addEventListener("click", function (e) {
+        if (e.target === modal || e.target === closeModalButton) {
+          modal.style.display = "none";
+          document.body.classList.remove("modal-open");
+        }
+      });
+    }, time);
+  }
+
+  bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close");
+  bindModal(".phone_link", ".popup", ".popup .popup_close");
+  showModal(".popup", 1000);
+};
+
+export default modals;
